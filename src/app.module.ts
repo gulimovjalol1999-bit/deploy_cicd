8bd5076411ef98a515df2ac2267423b6227d5fdd
Jaloliddin Gulimov <gulimovjalol1999-bit>
2026-05-18 12:56:41 +0500

    docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,9 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true })],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
