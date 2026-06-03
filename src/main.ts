f23f499650e800dbc841a17223de9a6ee6f9e678
Jaloliddin Gulimov <gulimovjalol1999-bit>
2026-06-03 13:23:12 +0500

    fix: remove helmet temporarily for http

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,24 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import helmet from 'helmet';
+// import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.use(helmet({ contentSecurityPolicy: false, hsts: false }));
+  // app.use(
+  //   helmet({
+  //     contentSecurityPolicy: {
+  //       directives: {
+  //         defaultSrc: [`'self'`],
+  //         styleSrc: [`'self'`, `'unsafe-inline'`],
+  //         scriptSrc: [`'self'`, `'unsafe-inline'`],
+  //         imgSrc: [`'self'`, 'data:', 'https:'],
+  //       },
+  //     },
+  //   }),
+  // );
 
   app.setGlobalPrefix('api/v1');
 
